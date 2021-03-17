9e09d2205a2b6e91af2e79077eb4474ef423889f
Momal Afzal <72850994+moomaal@users.noreply.github.com>
2021-03-17 20:21:57 +0500

    Updates

---
+++ precache-manifest.389ab6b0345df41d77a02916eb6e52ae.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ecc3035b0d1dbe180a",
+    "url": "/portfolio/static/css/main.4ea2617f.chunk.css"
+  },
+  {
+    "revision": "a6ecc3035b0d1dbe180a",
+    "url": "/portfolio/static/js/main.7a8aa0ce.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f0572d70e40f7ca06ca6",
+    "url": "/portfolio/static/js/2.e0dc5141.chunk.js"
+  },
+  {
+    "revision": "072dcbcadbe349464f5839f681efc267",
+    "url": "/portfolio/static/media/loogo.072dcbca.png"
+  },
+  {
+    "revision": "f069018c94294344381ba35d6170c446",
+    "url": "/portfolio/static/media/img-iian.f069018c.jpg"
+  },
+  {
+    "revision": "1d7900fc1138ed240940115f2d112a31",
+    "url": "/portfolio/static/media/imgg.1d7900fc.jpg"
+  },
+  {
+    "revision": "368bfa5f4a7fddd1d23e7fa45a83cdc0",
+    "url": "/portfolio/static/media/portfolio-01 copy.368bfa5f.jpg"
+  },
+  {
+    "revision": "4bf9bd7a8c81724a2cedc4d8dda08ad6",
+    "url": "/portfolio/static/media/portfolio-02 copy.4bf9bd7a.jpg"
+  },
+  {
+    "revision": "daa83bcb7cc724403d89736044e710ce",
+    "url": "/portfolio/static/media/portfolio-03 copy.daa83bcb.jpg"
+  },
+  {
+    "revision": "fe251a2085372f0d6ee065d56daa6dfe",
+    "url": "/portfolio/static/media/portfolio-04 copy.fe251a20.jpg"
+  },
+  {
+    "revision": "0a3172e92df3fe30a348f28e614175bb",
+    "url": "/portfolio/static/media/portfolio-05 copy.0a3172e9.jpg"
+  },
+  {
+    "revision": "b32b52df84bdf76ca03a6f3855ed770e",
+    "url": "/portfolio/static/media/portfolio-06 copy.b32b52df.jpg"
+  },
+  {
+    "revision": "6673fbd617212dd0b59ef742b7321a0b",
+    "url": "/portfolio/static/media/portfolio-07 copy.6673fbd6.jpg"
+  },
+  {
+    "revision": "4ac5cf257b327bab079ea4a5cb7fabd6",
+    "url": "/portfolio/static/media/portfolio-08 copy.4ac5cf25.jpg"
+  },
+  {
+    "revision": "7248a58a89b34d4bd9f5c09f36f91bc9",
+    "url": "/portfolio/index.html"
+  }
+];
